5e461afdee581d6b864dc11dbd3428a19984efd5
CrazzyChris <krzysztof.charzynski@ihs.com>
2015-11-24 15:39:13 +0100

    stupid indentation in sublime

--- ListCreator.rb
+++ ListCreator.rb
@@ -2,47 +2,47 @@ require './FillWithContent'
 
 class CreateListTest
 
-include FillElementWithContent
+  include FillElementWithContent
 
-def initialize(type,length)
+  def initialize(type,length)
 
-  case type
-  when "ordered"
-    @list = "<ol>"
-    for i in 1..length
-      @list += "<li outputclass=\"#{i}\">placeholder</li>"
+    case type
+    when "ordered"
+      @list = "<ol>"
+      for i in 1..length
+        @list += "<li outputclass=\"#{i}\">placeholder</li>"
+      end
+      @list += "</ol>"
+    when "unordered"
+      @list = "<ul>"
+      for i in 1..length
+        @list += "<li>placeholder</li>"
+      end
+      @list += "</ul>"
+    else
+      "not supported list type"
     end
-    @list += "</ol>"
-  when "unordered"
-    @list = "<ul>"
-    for i in 1..length
-      @list += "<li>placeholder</li>"
-    end
-    @list += "</ul>"
-  else
-    "not supported list type"
+
   end
 
-end
+  def return_list
+    @list
+  end
 
-def return_list
-  @list
-end
+  def fill_with_content
+    FillElementWithContent::fixed_content(@list,10)
+  end
 
-def fill_with_content
-  FillElementWithContent::fixed_content(@list,10)
-end
+  def fill_with_random_content
+    FillElementWithContent::random_content(@list,10)
+  end
 
-def fill_with_random_content
-  FillElementWithContent::random_content(@list,10)
-end
+  def fill_with_content_with_tc(tc_author)
+    FillElementWithContent::fixed_content_with_track_changes(@list,10,tc_author)
+  end
 
-def fill_with_content_with_tc(tc_author)
-  FillElementWithContent::fixed_content_with_track_changes(@list,10,tc_author)
-end
-
-def fill_with_random_content_with_tc(tc_author)
-  FillElementWithContent::random_content_with_track_changes(@list,10,tc_author)
-end
+  def fill_with_random_content_with_tc(tc_author)
+    FillElementWithContent::random_content_with_track_changes(@list,10,tc_author)
+  end
 
 end

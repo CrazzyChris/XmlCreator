38a28e332c43a4196160f22c6071cfdc48a4856c
CrazzyChris <krzysztof.charzynski@ihs.com>
2015-11-24 12:29:22 +0100

    track changes type is picked randomly

--- FillWithContent.rb
+++ FillWithContent.rb
@@ -21,25 +21,27 @@ module FillElementWithContent
 
   include CreateString
 
-    $fill = lambda do |string_length,random,tc,tc_type,tc_author|
+    $fill = lambda do |string_length,random,tc,tc_author|
       random ? string=create_string(rand(5..string_length)) : string=create_string(string_length)
-      tc ? add_track_changes_to_string(string,tc_type,tc_author) : string
+      tc_type = ["insertion","deletion"]
+      tc ? add_track_changes_to_string(string,tc_type.sample,tc_author) : string
+      #add randomizing tc_type
     end
 
   def self.random_content(content,string_length)
-    content.gsub!("placeholder") {|match| $fill.call(string_length,true,false,"","")}#{|placeholder| create_string(rand(5..string_length))}
+    content.gsub!("placeholder") {|match| $fill.call(string_length,true,false,"")}#{|placeholder| create_string(rand(5..string_length))}
   end
 
   def self.fixed_content(content,string_length)
-    content.gsub!("placeholder") {|match| $fill.call(string_length,false,false,"","")}
+    content.gsub!("placeholder") {|match| $fill.call(string_length,false,false,"")}
   end
 
-  def self.random_content_with_track_changes(content,string_length,tc_type = "insertion",tc_author="Ktest1")
-    content.gsub!("placeholder") {|match| $fill.call(string_length,true,true,tc_type,tc_author)}
+  def self.random_content_with_track_changes(content,string_length,tc_author="Ktest1")
+    content.gsub!("placeholder") {|match| $fill.call(string_length,true,true,tc_author)}
   end
 
-  def self.fixed_content_with_track_changes(content,string_length,tc_type = "insertion",tc_author="Ktest1")
-    content.gsub!("placeholder") {|match| $fill.call(string_length,false,true,tc_type,tc_author)}
+  def self.fixed_content_with_track_changes(content,string_length,tc_author="Ktest1")
+    content.gsub!("placeholder") {|match| $fill.call(string_length,false,true,tc_author)}
   end
 
 end
--- ListCreator.rb
+++ ListCreator.rb
@@ -38,11 +38,11 @@ def fill_with_random_content
 end
 
 def fill_with_content_with_tc(tc_type,tc_author)
-  FillElementWithContent::fixed_content_with_track_changes(@list,10,tc_type,tc_author)
+  FillElementWithContent::fixed_content_with_track_changes(@list,10,tc_author)
 end
 
 def fill_with_random_content_with_tc(tc_type,tc_author)
-  FillElementWithContent::random_content_with_track_changes(@list,10,tc_type,tc_author)
+  FillElementWithContent::random_content_with_track_changes(@list,10,tc_author)
 end
 
 end

da503312b025004ac2d830d1b92feef8660b6d1e
CrazzyChris <krzysztof.charzynski@ihs.com>
2015-11-25 11:52:00 +0100

    headers join lists and paragraphs

---
+++ HeaderCreator.rb
@@ -0,0 +1,31 @@
+require './FillWithContent'
+
+class CreateHeaderTest
+
+  include FillElementWithContent
+
+  def initialize(header_level)
+    "<h#{header_level}>placeholder</h#{header_level}>"
+  end
+
+  def return_header
+    @header
+  end
+
+  def fill_with_content
+    FillElementWithContent::fixed_content(@header,10)
+  end
+
+  def fill_with_random_content
+    FillElementWithContent::random_content(@header,10)
+  end
+
+  def fill_with_content_with_tc(tc_author)
+    FillElementWithContent::fixed_content_with_track_changes(@header,10,tc_author)
+  end
+
+  def fill_with_random_content_with_tc(tc_author)
+    FillElementWithContent::random_content_with_track_changes(@header,10,tc_author)
+  end
+
+end

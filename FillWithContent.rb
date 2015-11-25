8706a43c47f580441ef7c5afeb56581d85bfd889
CrazzyChris <krzysztof.charzynski@ihs.com>
2015-11-25 16:03:19 +0100

    remove redundant methods

--- FillWithContent.rb
+++ FillWithContent.rb
@@ -1,25 +1,4 @@
-module TrackChanges
-
-  def add_track_changes_to_string(string,type,author)
-    track_changes_start = rand(1..string.length/2)
-    track_changes_end = rand(track_changes_start..string.length)
-    case type
-    when 'insertion'
-      string.insert(track_changes_end,"<xt:insText xmlns:xt=\"urn:xpressauthor:trackchanges\" xt:action=\"end\"/>")
-      string.insert(track_changes_start, "<xt:insText xmlns:xt=\"urn:xpressauthor:trackchanges\" xt:action=\"start\" xt:author=\"Test1\"/>")
-    when 'deletion'
-      string.insert(track_changes_end,"<xt:delText xmlns:xt=\"urn:xpressauthor:trackchanges\" xt:action=\"end\"/>")
-      string.insert(track_changes_start, "<xt:delText xmlns:xt=\"urn:xpressauthor:trackchanges\" xt:action=\"start\" xt:author=\"Test1\"/>")
-    else
-      abort("Such track changes type (#{type}) is not supported!")
-    end
-  end
-
-end
-
 module FillElementWithContent
-
-  #include CreateString
 
     $fill = lambda do |string_length,random,tc,tc_author|
       random ? string=create_string(rand(5..string_length)) : string=create_string(string_length)
